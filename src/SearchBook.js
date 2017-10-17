546500a1b63685aa120f2dbb9fc53a948932bae5
Samantha Degges <samanthadegges@gmail.com>
2017-10-17 16:40:35 -0700

    FIX book.shelf now defaults to 'none'

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -43,7 +43,7 @@ class SearchBook extends React.Component {
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 200, backgroundImage: `url(${book.imageLinks?book.imageLinks.thumbnail:'http://via.placeholder.com/128x193?text=No%20Cover'})`}}></div>
                 <div className="book-shelf-changer">
-                  <select value={book.shelf} onChange={(event)=>onChangeShelf(book,event.target.value)}>
+                  <select value={book.shelf ? book.shelf: "none"} onChange={(event)=>onChangeShelf(book,event.target.value)}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
